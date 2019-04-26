446ebe31b89270326ab25680ab69851a12fa36a8
tkv09 <tarun072v@outlook.com>
2019-04-26 19:58:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ else
 </head>
 <body>
     <section class="congratulations">
-        <h2>Team Congratulations!</h2>
+        <h1>Congratulations!</h1>
         <p>Your AWS Elastic Beanstalk (EBS) <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
